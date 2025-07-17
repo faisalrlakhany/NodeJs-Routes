a4a83da9162dac09c5c243d97603a1423f0c6acf
faisalrlakhany <lakhanyfaisal37@gmail.com>
2025-07-17 19:26:08 +0500

    user route completed

--- routers/course.js
+++ routers/course.js
@@ -1,2 +1,31 @@
 
 import express from "express";
+
+const router = express.Router()
+
+const courses = [
+    {
+        title : "Math",
+        description : "I love Math",
+        duration : "1 Year"
+    }
+]
+
+
+router.get('/', (req , res)=>{
+
+    res.status(200).json({
+        error:false,
+        data:courses,
+        msg:"All courses data fetched successfully"
+    })
+
+})
+
+
+router.post('/',(req,res)=>{
+
+    const {title , description , duration} = req.body
+
+    courses.push(title,description,duration)
+})
